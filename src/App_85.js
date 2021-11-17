197b1fa5fbb5bf6cb76d84b5ef3976c52983de1a
tsw0603 <209410785@gms.tku.edu.tw>
2021-11-17 13:03:12 +0800

    first commit

---
+++ src/App_85.js
@@ -0,0 +1,12 @@
+import './App_85.scss';
+import Homepage_85 from './pages/Homepage_85';
+
+const App_85 = () => {
+  return (
+    <div>
+      <Homepage_85/>
+    </div>
+  );
+}
+
+export default App_85;
---
+++ src/components/Header_85.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './Header_85.scss';
+
+const Header_85 = () => {
+    return (
+  
+               <div className="header">
+      <div className="header__container">
+        <div className="header__main">
+          <div className="header__logo">
+            <a ref="#"></a>
+          </div>
+          <div className="header__item">
+            <ul className="header__list">
+              <li><a>咖啡</a></li>
+              <li><a href="./pdoducts.html">商品</a></li>
+              <li><a>門市</a></li>
+              <li><a>星禮程</a></li>
+              <li><a>第四生活空間</a></li>
+              <li><a>送禮專區</a></li>
+              <li><a>線上門市</a></li>
+            </ul>
+          </div>
+        </div>
+        <div className="header__sub"><a ref="#"></a></div>
+      </div>
+    </div>
+
+    )
+    
+}
+
+export default Header_85;
