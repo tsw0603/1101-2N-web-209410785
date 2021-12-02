93aaecac54b91bb0d5584b0e1160618bca465348
tsw0603 <209410785@gms.tku.edu.tw>
2021-12-02 17:02:37 +0800

    repd

--- src/App_85.js
+++ src/App_85.js
@@ -3,6 +3,11 @@ import Homepage_85 from './pages/Homepage_85';
 import Header_85 from './components/Header_85';
 import Footer_85 from './components/Footer_85';
 import Product_85 from './pages/ProductPage_85';
+import Product1_85 from './pages/ProductPage1_85';
+import Product2_85 from './pages/ProductPage2_85';
+import Product3_85 from './pages/ProductPage3_85';
+import Product4_85 from './pages/ProductPage4_85';
+import Product5_85 from './pages/ProductPage5_85';
 import Coffee_85 from './pages/CoffeePage_85';
 import Store_85 from './pages/StorePage_85';
 import StarCourtesy_85 from './pages/StarCourtesyPage_85';
@@ -20,6 +25,11 @@ const App_85 = () => {
         <Route exact path='/' component={Homepage_85} />
         <Route exact path='/CoffeePage_85' component={Coffee_85} />
         <Route exact path='/Product_85' component={Product_85} />
+        <Route exact path='/Product1_85' component={Product1_85} />
+        <Route exact path='/Product2_85' component={Product2_85} />
+        <Route exact path='/Product3_85' component={Product3_85} />
+        <Route exact path='/Product4_85' component={Product4_85} />
+        <Route exact path='/Product5_85' component={Product5_85} />
         <Route exact path='/StorePage_85' component={Store_85} />
         <Route exact path='/StarCourtesyPage_85' component={StarCourtesy_85} />
         <Route exact path='/FourthSpacePage_85' component={FourthSpace_85} />
